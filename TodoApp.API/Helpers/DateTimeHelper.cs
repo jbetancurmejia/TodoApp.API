71d0ec446872d43415ebaba54084c642924584c4
camilo betancur <jcbmdeveloper@gmail.com>
2024-06-24 21:24:37 -0500

    Add project files.

---
+++ TodoApp.API/Helpers/DateTimeHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApp.API.Helpers
+{
+    public static class DateTimeHelper
+    {
+        private static readonly TimeZoneInfo colombiaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("SA Pacific Standard Time");
+
+        public static DateTime LocalDateNow()
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, colombiaTimeZone);
+        }
+    }
+}
